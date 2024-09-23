c97e55931e5bffac39019dafb45e73aebcd71078
Aabir <ahmedabir_official01@outlook.com>
2024-09-24 00:07:55 +0200

    managing the amount using js for the nuwakhali section

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,11 @@
- const blogPage = document.getElementById('blog-page');
+function getElementByIdValue (id){
+    return parseFloat(document.getElementById(id).value);
+}
 
+
+ 
+//  navigate the 2 html file start 
+ const blogPage = document.getElementById('blog-page');
  if (blogPage){
     blogPage.addEventListener('click', function(event){
         event.preventDefault();
@@ -7,13 +13,44 @@
     })
  }
  const homePage = document.getElementById('home-page');
-
  if (homePage){
     homePage.addEventListener('click', function(event){
         event.preventDefault();
         window.location.href  ='/index.html'
     })
  }
+//  navigate the 2 html file end 
+
+// donate button make manupulate while using add addEventListener  start 
+
+const donateMoney = document.getElementById('btn-donate');
+donateMoney.addEventListener('click', function(){
+    const amountInput =  getElementByIdValue('doneted-tk-nuwakhali');
+    
+    
+     
+   const nuwakhaliTk=parseFloat(document.getElementById('nuwakhali-tk').innerText);
+   const balance=parseFloat(document.getElementById('main-balance').innerText);
+
+   const  currentBalance = balance-amountInput;
+   document.getElementById('main-balance').innerText = currentBalance.toFixed(2);
+
+    const nuwakhaliAdding = amountInput + nuwakhaliTk;
+    document.getElementById('nuwakhali-tk').innerText = nuwakhaliAdding.toFixed(2);
+
+
+     
+    document.getElementById('doneted-tk-nuwakhali') = '';
+     
+
+     
+
+     
+})
+
+
+
+// donate button make manupulate while using add addEventListener  end  
 
  
  
