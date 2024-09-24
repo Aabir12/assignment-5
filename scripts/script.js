f6c90001165704603c8db793bb1890f9afb41a84
Aabir <ahmedabir_official01@outlook.com>
2024-09-24 13:13:27 +0200

    adding the div when the user click the donate btn

--- scripts/script.js
+++ scripts/script.js
@@ -2,6 +2,30 @@ function getElementByIdValue (id){
     return parseFloat(document.getElementById(id).value);
 }
 
+function modal (id){
+   const modal = document.getElementById(id).showModal();
+   return modal
+}
+
+function addHistory (cz, location, amount){
+    const historyTab = document.getElementById('history-tap') ;
+    const insertDiv = document.createElement('div');
+    insertDiv.classList.add('border', 'border-zinc-300', 'p-5', 'mb-5', 'rounded-lg');
+    //  creat p tag inside the insert div 
+    const information = document.createElement('p');
+    information.innerText = `${amount.toFixed(2)}Taka is donated for ${cz} at ${location}`;
+    // time information 
+    const dataInformation = document.createElement('p');
+    const timeInformation = new Date();
+    dataInformation.innerText = `Date : ${timeInformation}`
+
+    // append the child 
+    insertDiv.appendChild(information);
+    insertDiv.appendChild(dataInformation);
+    historyTab.appendChild(insertDiv);
+    
+
+}
 
  
 //  navigate the 2 html file start 
@@ -10,6 +34,7 @@ function getElementByIdValue (id){
     blogPage.addEventListener('click', function(event){
         event.preventDefault();
         window.location.href  ='/blog.html'
+         
     })
  }
  const homePage = document.getElementById('home-page');
@@ -27,6 +52,14 @@ const donateMoney = document.getElementById('btn-donate');
 donateMoney.addEventListener('click', function(){
     const amountInput =  getElementByIdValue('doneted-tk-nuwakhali');
     
+    if(amountInput<0 || isNaN(amountInput) || typeof amountInput !== 'number'){
+        return alert('please make sure that you provide a positive number')
+        
+    }
+     if(amountInput){
+        modal('my_modal')
+     }
+   
     
      
    const nuwakhaliTk=parseFloat(document.getElementById('nuwakhali-tk').innerText);
@@ -37,14 +70,10 @@ donateMoney.addEventListener('click', function(){
 
     const nuwakhaliAdding = amountInput + nuwakhaliTk;
     document.getElementById('nuwakhali-tk').innerText = nuwakhaliAdding.toFixed(2);
+    document.getElementById('doneted-tk-nuwakhali').value = '';
 
-
-     
-    document.getElementById('doneted-tk-nuwakhali') = '';
-     
-
-     
-
+    // add history 
+    addHistory ('flood', 'nowakhali, bangladesh', amountInput)
      
 })
 
